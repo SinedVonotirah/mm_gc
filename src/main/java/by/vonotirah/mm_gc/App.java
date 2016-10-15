f4881b7defd0f886a6aa844781d65ba4aa80a3f6
SinedVonotirah <sined.vonotirah@gmail.com>
2016-10-15 11:41:56 +0300

    Clear project

---
+++ src/main/java/by/vonotirah/mm_gc/App.java
@@ -0,0 +1,13 @@
+package by.vonotirah.mm_gc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
